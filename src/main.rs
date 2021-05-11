4afda7438ef8ccdafdb06d96ba97f6309b445551
Phil Howlett <phil.g.howlett@gmail.com>
2021-05-12 09:21:40 +1000

    A simple console window with FPS counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod prelude {
+    pub use bracket_lib::prelude::*;
+}
+
+use prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(0, 0, format!("FPS: {}", ctx.fps));
+        ctx.print_centered(1, "Movie Monsters");
+    }
+}
+
+// Main entry point
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Movie Monsters")
+        .with_dimensions(80, 50)
+        .with_fps_cap(30.0)
+        .build()?;
+
+    let gs: State = State {};
+
+    main_loop(context, gs)
 }
