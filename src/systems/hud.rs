86d93ea95e82e679a047f7695eb013a60309bbff
Phil Howlett <phil.g.howlett@gmail.com>
2021-05-15 09:25:20 +1000

    Tweaking the HUD layout

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -9,19 +9,19 @@ pub fn hud() {
     draw_batch.target(2);
 
     // Main Map is the left most 60 columns leaving 58 usable columns
-    // draw_batch.draw_double_box(
-    //     Rect::with_size(0, 0, 59, 46),
-    //     ColorPair::new(DARKBLUE, BLACK),
-    // );
+    draw_batch.draw_hollow_box(
+        Rect::with_size(0, 0, 59, 46),
+        ColorPair::new(DARKBLUE, BLACK),
+    );
 
     // Info Panel is the right most 20 columns leaving 18 usable columns
-    draw_batch.draw_double_box(
+    draw_batch.draw_hollow_box(
         Rect::with_size(60, 0, 19, 46),
         ColorPair::new(DARKBLUE, BLACK),
     );
 
     // Bottom Footer are the bottom 3 lines of the screen leaving 1 usable row
-    draw_batch.draw_double_box(
+    draw_batch.draw_hollow_box(
         Rect::with_size(0, 47, 79, 2),
         ColorPair::new(DARKBLUE, BLACK),
     );
